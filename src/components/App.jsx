d3f62a412a6ba7bea4fd7fed1d1c5d129673690e
Olvet7 <volya.maslova@gmail.com>
2024-01-17 21:14:56 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,79 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, {Component} from "react";
+import { nanoid } from "nanoid";
+import { Notify } from "notiflix";
+import ContactForm from "./ContactForm";
+import ContactList from "./ContactList";
+import Filter from "./Filter";
+import styles from './App.module.css'; 
+
+class App extends Component {
+  state = {
+    contacts: [{
+      id: nanoid(),
+      name: "Olha",
+      number: 9379992
+    }],
+    filter: "",
+  }
+
+  addContact = (data) => {
+    const newContact = { id: nanoid(), ...data };
+    this.state.contacts.some(({ name }) => {
+      return name.toLocaleLowerCase() === newContact.name.toLocaleLowerCase();
+    })
+      ? Notify.failure(`${newContact.name} is already in contacts`, {
+        width: '230px',
+        timeout: 5000,
+        fontSize: '16px',
+      })
+      : this.setState(({ contacts }) => {
+          return { contacts: [...contacts, newContact] };
+        });
+  }
+
+  deleteContact = (id) => {
+    this.setState(({contacts}) => {
+      const newContacts = contacts.filter(item => item.id !== id);
+
+      return {
+        contacts: newContacts,
+      }
+    })
+  }
+
+  changeFilter = ({target}) => {
+    console.log('target :>> ', target);
+    this.setState({
+      filter: target.value,
+    })
+  }
+
+  getFilteredContact() {
+    const {filter, contacts} = this.state;
+
+    const normalizedFilter = filter.toLowerCase();
+
+    const filteredContacts = contacts.filter(({name, number}) => { 
+      const normalizedName = name.toLowerCase();
+      const normalizedNumber = typeof number === 'number' ? number.toString() : number.toLowerCase();
+      return (normalizedName.includes(normalizedFilter) || normalizedNumber.includes(normalizedFilter))
+    })
+    return filteredContacts;
+  }
+
+  render () {
+    const {filter} = this.state; 
+    const {addContact, deleteContact, changeFilter} = this;
+    const contacts = this.getFilteredContact();
+
+    return (
+      <div className={styles.section}>
+        <ContactForm onSubmit={addContact}/>
+        <Filter onFilter={changeFilter} filter={filter}/>     
+        <ContactList contacts={contacts} deleteContact={deleteContact}/>
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,25 @@
+import { Component } from "react";
+import PropTypes from 'prop-types';
+import styles from './filter.module.css'
+
+class Filter extends Component {
+	static propTypes = {
+		onFilter: PropTypes.func.isRequired,
+		filter: PropTypes.string,
+	};
+
+	render() {
+		const { filter, onFilter } = this.props;
+		return (
+			<div className={styles.filter}>
+				<p className={styles.title}>Find contacts by name</p>
+				<input 
+				placeholder="Search"
+				type="text" 
+				onChange={onFilter} value={filter}  />
+			</div>
+		)
+	}
+}
+
+export default Filter;
